61f93f9cd8f39ddf8b5a9e43034527a0229ab807
Francisco Preller <francisco@flamingo.io>
2017-03-09 21:35:38 +1100

    pointless formatting commit

--- src/lib/connector.js
+++ src/lib/connector.js
@@ -10,13 +10,14 @@ const fs = require('fs');
 class Connector {
     constructor(connection, socket) {
         this.socket = socket;
-        this.host   = connection.host;
-        this.port   = connection.port;
-        this.uuid   = connection.uuid;
-        this.input  = new TelnetInput();
+        this.host = connection.host;
+        this.port = connection.port;
+        this.uuid = connection.uuid;
+        this.input = new TelnetInput();
         this.output = new TelnetOutput();
-        this.data   = '';
-        this.conn   = null;
+        this.data = '';
+        this.buffer = '';
+        this.conn = null;
         this.connected = false;
         this.timestamp = Date.now();
 
@@ -26,36 +27,75 @@ class Connector {
         this.bindConnectionBinding();
     }
     connect(callback) {
-        this.conn = net.createConnection(this.port, this.host, (err) => {
-            if (err) {
-                return callback(err, null);
-            }
+        this.conn = net
+            .createConnection(this.port, this.host, (err) => {
+                if (err) {
+                    return callback(err, null);
+                }
 
-            // Bind IO
-            this.conn.pipe(this.input);
-            this.output.pipe(this.conn);
+                // Bind IO
+                this.conn.pipe(this.input);
+                this.output.pipe(this.conn);
 
-            callback(null, {
-                host: this.host,
-                port: this.port,
-                uuid: this.uuid,
+                callback(null, {
+                    host: this.host,
+                    port: this.port,
+                    uuid: this.uuid,
+                });
             });
+
+        this.conn.on('close', () => {
+            console.log(`(${this.host}) lost connection...`);
+            this.conn.unpipe(this.input);
+            this.output.unpipe(this.conn);
+        });
+        this.conn.on('error', (err) => {
+            console.log('Connection error', err);
         });
     }
     bindCommandHandler() {
         this.socket.on(`server.command.${this.uuid}`, (data) => {
-            console.log('Received command for server:', data.command);
             this.processCommand(data.command);
         });
     }
     bindOutputProcessing() {
         this.input.on('data', (data) => {
-            this.processOutput(data);
+            // Buffer output until last character is a carriege return
+            this.buffer += data;
+            const lastChar = data[data.length - 1];
+            if (lastChar === 32 && !(data[data.length - 2] && data[data.length - 2] === 32)) {
+                this.processOutput(this.buffer);
+                this.buffer = '';
+            }
+        });
+        this.input.on('command', (command) => {
+            // Received: IAC <command> - See RFC 854
+            console.log(`(${this.host}) got command:`, command);
+        });
+        this.input.on('do', (option) => {
+            // Received: IAC DO <option> - See RFC 854
+            console.log(`(${this.host}) got do:`, option);
+        });
+        this.input.on('dont', (option) => {
+            // Received: IAC DONT <option> - See RFC 854
+            console.log(`(${this.host}) got dont:`, option);
+        });
+        this.input.on('sub', (option, buffer) => {
+            // Received: IAC SB <option> <buffer> IAC SE - See RFC 855
+            console.log(`(${this.host}) got sub:`, option, buffer);
+        });
+        this.input.on('will', (option) => {
+            // Received: IAC WILL <option> - See RFC 854
+            console.log(`(${this.host}) got will:`, option);
+        });
+        this.input.on('wont', (option) => {
+            // Received: IAC WONT <option> - See RFC 854
+            console.log(`(${this.host}) got wont:`, option);
         });
     }
     bindConnectionBinding() {
         this.socket.on(`server.connected.${this.uuid}`, () => {
-            console.log(`Client connected to ${this.host}...`);
+            console.log(`(${this.host}) connected...`);
             this.connected = true;
             // If we have data in cache after connecting, send it
             if (this.data !== '') {
@@ -65,16 +105,15 @@ class Connector {
         });
     }
     sendOutput(output) {
-        this.logOutputToFile(output.toString());
         const lines = ansiHTML.toLineObjects({ str: output.toString() });
         const emitString = `server.output.${this.uuid}`;
-
         this.socket.emit('server.output', { lines, uuid: this.uuid });
     }
     processOutput(output) {
         if (!this.connected || this.data !== '') {
             this.data += output;
         } else {
+            this.logOutputToFile(output.toString());
             this.sendOutput(output)
         }
     }
@@ -85,15 +124,12 @@ class Connector {
         const fileName = `outflow_${this.host}_${this.timestamp}.log`;
         const file = `${global.logPath}/${fileName}`;
         fs.open(file, 'a+', (err, fd) => {
-            console.log('Logging to file:', file);
             if (err) {
                 console.error(`Could not open file ${file}:`, err.message);
             } else {
                 fs.write(fd, output, { encoding: 'utf8' }, (err) => {
                     if (err) {
                         console.error(`Could not write to file ${file}`, err.message);
-                    } else {
-                        console.log('Logged successful');
                     }
                 });
             }
