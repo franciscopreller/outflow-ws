0ce7454199fc6b570599d5223246319003597d3b
Francisco Preller <francisco.preller@gmail.com>
2017-03-25 12:19:47 +1100

    Clean up connector for use with react/redux

--- src/events.js
+++ src/events.js
@@ -1,13 +1,13 @@
 const TelnetConnector = require('./lib/connector');
 
 module.exports.bindWebsocketHandlers = (socket) => {
-  socket.on('open.connection', (data, callback) => {
+  socket.on('connection.open', (data) => {
     if (data.payload && data.payload.connection) {
       const connection = data.payload.connection;
       console.log('Connection request, connecting to...', connection);
 
       const Connector = new TelnetConnector(connection, socket);
-      Connector.connect(callback);
+      Connector.connect();
     } else {
       console.error('No data received for server connection...');
     }
--- src/lib/connector.js
+++ src/lib/connector.js
@@ -11,36 +11,31 @@ class Connector {
     this.host = connection.host;
     this.port = connection.port;
     this.uuid = connection.uuid;
-    this.input = new TelnetInput();
-    this.output = new TelnetOutput();
     this.data = '';
     this.buffer = '';
     this.conn = null;
-    this.connected = false;
     this.timestamp = Date.now();
+
+    // Telnet handlers
+    this.input = new TelnetInput();
+    this.output = new TelnetOutput();
 
     // Bindings
     this.bindOutputProcessing();
     this.bindCommandHandler();
-    this.bindConnectionBinding();
   }
 
-  connect(callback) {
+  connect() {
     this.conn = net
       .createConnection(this.port, this.host, (err) => {
         if (err) {
-          return callback(err, null);
+          // send error
+          console.error(err);
         }
 
         // Bind IO
         this.conn.pipe(this.input);
         this.output.pipe(this.conn);
-
-        callback(null, {
-          host: this.host,
-          port: this.port,
-          uuid: this.uuid,
-        });
       });
 
     this.conn.on('close', () => {
@@ -55,80 +50,70 @@ class Connector {
 
   bindCommandHandler() {
     this.socket.on(`server.command.${this.uuid}`, (data) => {
-      this.processCommand(data.command);
+      this.receiveCommand(data.command);
     });
   }
 
   bindOutputProcessing() {
     this.input.on('data', (data) => {
-      // Buffer output until last character is a carriege return
+      // Buffer output until last two characters are carriage returns
       this.buffer += data;
       const lastChar = data[data.length - 1];
-      if (lastChar === 32 && !(data[data.length - 2] && data[data.length - 2] === 32)) {
-        this.processOutput(this.buffer);
+      const secondLastChar = (data[data.length - 1]) ? data[data.length - 1] : null;
+
+      // On two consecutive carriage returns, process the output and cear the buffer
+      if (lastChar === 32 && secondLastChar === 32) {
+        this.sendOutput(this.buffer);
         this.buffer = '';
       }
     });
+
+    // Telnet events
     this.input.on('command', (command) => {
       // Received: IAC <command> - See RFC 854
       console.log(`(${this.host}) got command:`, command);
     });
+
     this.input.on('do', (option) => {
       // Received: IAC DO <option> - See RFC 854
       console.log(`(${this.host}) got do:`, option);
     });
+
     this.input.on('dont', (option) => {
       // Received: IAC DONT <option> - See RFC 854
       console.log(`(${this.host}) got dont:`, option);
     });
+
     this.input.on('sub', (option, buffer) => {
       // Received: IAC SB <option> <buffer> IAC SE - See RFC 855
       console.log(`(${this.host}) got sub:`, option, buffer);
     });
+
     this.input.on('will', (option) => {
       // Received: IAC WILL <option> - See RFC 854
       console.log(`(${this.host}) got will:`, option);
     });
+
     this.input.on('wont', (option) => {
       // Received: IAC WONT <option> - See RFC 854
       console.log(`(${this.host}) got wont:`, option);
     });
-  }
 
-  bindConnectionBinding() {
-    this.socket.on(`server.connected.${this.uuid}`, () => {
-      console.log(`(${this.host}) connected...`);
-      this.connected = true;
-      // If we have data in cache after connecting, send it
-      if (this.data !== '') {
-        this.sendOutput(this.data);
-        this.data = '';
-      }
-    });
   }
 
   sendOutput(output) {
     const lines = ansiHTML.toLineObjects({str: output.toString()});
-    const emitString = `server.output.${this.uuid}`;
-    this.socket.emit('server.output', {lines, uuid: this.uuid});
+    console.log('Sending output', output.toString());
+    this.socket.emit('connection.output', {lines, uuid: this.uuid});
   }
 
-  processOutput(output) {
-    if (!this.connected || this.data !== '') {
-      this.data += output;
-    } else {
-      this.logOutputToFile(output.toString());
-      this.sendOutput(output)
-    }
-  }
-
-  processCommand(command) {
+  receiveCommand(command) {
     this.output.write(command + '\n');
   }
 
   logOutputToFile(output) {
     const fileName = `outflow_${this.host}_${this.timestamp}.log`;
-    const file = `${global.logPath}/${fileName}`;
+    const file = `/${fileName}`;
     fs.open(file, 'a+', (err, fd) => {
       if (err) {
         console.error(`Could not open file ${file}:`, err.message);
